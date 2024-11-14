eb50e602cb02de1436fdc8bc25c412e1b34e4ce5
Nguyễn Trọng Long <longnt14042004@gmail.com>
2024-11-14 11:50:48 +0700

    feat(ui)-nav-user&store-userInfo

--- src/views/layouts/instructor-sidebar.vue
+++ src/views/layouts/instructor-sidebar.vue
@@ -6,7 +6,7 @@
           <div class="profile-bg">
             <div class="profile-img">
               <router-link to="/instructor/instructor-profile"
-              ><img :src="user?.avatarUrl" alt="Img" class="object-fit-cover"
+                ><img :src="user?.avatarUrl" alt="Img" class="object-fit-cover"
               /></router-link>
             </div>
           </div>
@@ -14,14 +14,14 @@
             <div class="profile-name text-center">
               <h4>
                 <router-link to="/instructor/instructor-profile"
-                >Eugene Andre</router-link
+                  >Eugene Andre</router-link
                 >
               </h4>
               <p>Instructor</p>
               <router-link
-                  to="/course/add-course"
-                  class="add-course btn-primary"
-              >Add New Course</router-link
+                to="/course/add-course"
+                class="add-course btn-primary"
+                >Add New Course</router-link
               >
             </div>
           </div>
@@ -32,115 +32,115 @@
           <h3>Dashboard</h3>
           <ul>
             <li
-                class="nav-item"
-                :class="{ active: isActive('/instructor/instructor-dashboard') }"
+              class="nav-item"
+              :class="{ active: isActive('/instructor/instructor-dashboard') }"
             >
               <router-link
-                  to="/instructor/instructor-dashboard"
-                  class="nav-link"
+                to="/instructor/instructor-dashboard"
+                class="nav-link"
               >
                 <i class="bx bxs-tachometer"></i>Dashboard
               </router-link>
             </li>
             <li
-                class="nav-item"
-                :class="{ active: isActive('/instructor/instructor-profile') }"
+              class="nav-item"
+              :class="{ active: isActive('/instructor/instructor-profile') }"
             >
               <router-link to="/instructor/instructor-profile" class="nav-link">
                 <i class="bx bxs-user"></i>My Profile
               </router-link>
             </li>
             <li
-                class="nav-item"
-                :class="{
+              class="nav-item"
+              :class="{
                 active: isActive('/instructor/instructor-enrolled-course'),
               }"
             >
               <router-link
-                  to="/instructor/instructor-enrolled-course"
-                  class="nav-link"
+                to="/instructor/instructor-enrolled-course"
+                class="nav-link"
               >
                 <i class="bx bxs-graduation"></i>Enrolled Courses
               </router-link>
             </li>
             <li
-                class="nav-item"
-                :class="{ active: isActive('/instructor/instructor-wishlist') }"
+              class="nav-item"
+              :class="{ active: isActive('/instructor/instructor-wishlist') }"
             >
               <router-link
-                  to="/instructor/instructor-wishlist"
-                  class="nav-link"
+                to="/instructor/instructor-wishlist"
+                class="nav-link"
               >
                 <i class="bx bxs-heart"></i>Wishlist
               </router-link>
             </li>
             <li
-                class="nav-item"
-                :class="{ active: isActive('/instructor/instructor-reviews') }"
+              class="nav-item"
+              :class="{ active: isActive('/instructor/instructor-reviews') }"
             >
               <router-link to="/instructor/instructor-reviews" class="nav-link">
                 <i class="bx bxs-star"></i>Reviews
               </router-link>
             </li>
             <li
-                class="nav-item"
-                :class="{ active: isActive('/instructor/instructor-quiz') }"
+              class="nav-item"
+              :class="{ active: isActive('/instructor/instructor-quiz') }"
             >
               <router-link to="/instructor/instructor-quiz" class="nav-link">
                 <i class="bx bxs-shapes"></i>My Quiz Attempts
               </router-link>
             </li>
             <li
-                class="nav-item"
-                :class="{ active: isActive('/instructor/instructor-orders') }"
+              class="nav-item"
+              :class="{ active: isActive('/instructor/instructor-orders') }"
             >
               <router-link to="/instructor/instructor-orders" class="nav-link">
                 <i class="bx bxs-cart"></i>Order History
               </router-link>
             </li>
             <li
-                class="nav-item"
-                :class="{ active: isActive('/instructor/instructor-qa') }"
+              class="nav-item"
+              :class="{ active: isActive('/instructor/instructor-qa') }"
             >
               <router-link to="/instructor/instructor-qa" class="nav-link">
                 <i class="bx bxs-bookmark-alt"></i>Question & Answer
               </router-link>
             </li>
             <li
-                class="nav-item"
-                :class="{ active: isActive('/instructor/instructor-referral') }"
+              class="nav-item"
+              :class="{ active: isActive('/instructor/instructor-referral') }"
             >
               <router-link
-                  to="/instructor/instructor-referral"
-                  class="nav-link"
+                to="/instructor/instructor-referral"
+                class="nav-link"
               >
                 <i class="bx bxs-user-plus"></i>Referrals
               </router-link>
             </li>
             <li
-                class="nav-item"
-                :class="{ active: isActive('/instructor/instructor-chat') }"
+              class="nav-item"
+              :class="{ active: isActive('/instructor/instructor-chat') }"
             >
               <router-link to="/instructor/instructor-chat" class="nav-link">
                 <i class="bx bxs-chat"></i>Messages
               </router-link>
             </li>
             <li
-                class="nav-item"
-                :class="{
+              class="nav-item"
+              :class="{
                 active: isActive('/instructor/instructor-notifications'),
               }"
             >
               <router-link
-                  to="/instructor/instructor-notifications"
-                  class="nav-link"
+                to="/instructor/instructor-notifications"
+                class="nav-link"
               >
                 <i class="bx bxs-bell"></i>Notifications
               </router-link>
             </li>
             <li
-                class="nav-item"
-                :class="{ active: isActive('/instructor/instructor-tickets') }"
+              class="nav-item"
+              :class="{ active: isActive('/instructor/instructor-tickets') }"
             >
               <router-link to="/instructor/instructor-tickets" class="nav-link">
                 <i class="bx bxs-coupon"></i>Support Tickets
@@ -150,68 +150,68 @@
           <h3>Instructor</h3>
           <ul>
             <li
-                class="nav-item"
-                :class="{ active: isActive('/instructor/instructor-course') }"
+              class="nav-item"
+              :class="{ active: isActive('/instructor/instructor-course') }"
             >
               <router-link to="/instructor/instructor-course" class="nav-link">
                 <i class="bx bxs-rocket"></i>My Courses
               </router-link>
             </li>
             <li
-                class="nav-item"
-                :class="{
+              class="nav-item"
+              :class="{
                 active: isActive('/instructor/instructor-announcements'),
               }"
             >
               <router-link
-                  to="/instructor/instructor-announcements"
-                  class="nav-link"
+                to="/instructor/instructor-announcements"
+                class="nav-link"
               >
                 <i class="bx bxs-volume-full"></i>Announcements
               </router-link>
             </li>
             <li
-                class="nav-item"
-                :class="{ active: isActive('/instructor/instructor-withdraw') }"
+              class="nav-item"
+              :class="{ active: isActive('/instructor/instructor-withdraw') }"
             >
               <router-link
-                  to="/instructor/instructor-withdraw"
-                  class="nav-link"
+                to="/instructor/instructor-withdraw"
+                class="nav-link"
               >
                 <i class="bx bxs-wallet"></i>Withdrawls
               </router-link>
             </li>
             <li
-                class="nav-item"
-                :class="{
+              class="nav-item"
+              :class="{
                 active: isActive('/instructor/instructor-quiz-attempts'),
               }"
             >
               <router-link
-                  to="/instructor/instructor-quiz-attempts"
-                  class="nav-link"
+                to="/instructor/instructor-quiz-attempts"
+                class="nav-link"
               >
                 <i class="bx bxs-shapes"></i>Quiz Attempts
               </router-link>
             </li>
             <li
-                class="nav-item"
-                :class="{ active: isActive('/instructor/instructor-assignment') }"
+              class="nav-item"
+              :class="{ active: isActive('/instructor/instructor-assignment') }"
             >
               <router-link
-                  to="/instructor/instructor-assignment"
-                  class="nav-link"
+                to="/instructor/instructor-assignment"
+                class="nav-link"
               >
                 <i class="bx bxs-file"></i>Assignments
               </router-link>
             </li>
             <li
-                class="nav-item"
-                :class="{ active: isActive('/instructor/instructor-earnings') }"
+              class="nav-item"
+              :class="{ active: isActive('/instructor/instructor-earnings') }"
             >
               <router-link
-                  to="/instructor/instructor-earnings"
-                  class="nav-link"
+                to="/instructor/instructor-earnings"
+                class="nav-link"
               >
                 <i class="bx bxs-badge-dollar"></i>Earnings
               </router-link>
@@ -220,12 +220,12 @@
           <h3>Account Settings</h3>
           <ul>
             <li
-                class="nav-item"
-                :class="{ active: isActive('/instructor/instructor-settings') }"
+              class="nav-item"
+              :class="{ active: isActive('/instructor/instructor-settings') }"
             >
               <router-link
-                  to="/instructor/instructor-settings"
-                  class="nav-link"
+                to="/instructor/instructor-settings"
+                class="nav-link"
               >
                 <i class="bx bxs-cog"></i>Settings
               </router-link>
@@ -242,12 +242,27 @@
   </div>
 </template>
 <script>
-import { ref } from "vue";
-import {useStore} from "vuex";
+import { ref, onMounted } from "vue";
+import baseApi from "@/axios";
 export default {
   data() {
-    const store = useStore();
-    const user = ref(store.state.userInfo);
+    const user = ref(null);
+
+    function getUserInfo() {
+      baseApi
+          .get("/users/myInfo")
+          .then((response) => {
+            user.value = response.data.result;
+          })
+          .catch((error) => {
+            console.error("Error during introspection:", error);
+          });
+      return user;
+    }
+
+    onMounted(() => {
+      getUserInfo()
+    });
 
     return {
       activeClass: "active",
